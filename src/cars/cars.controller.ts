c958af31f03c8f8e78b664c8fb0cc88aed3f40e4
JhonGarc <Jhon61631@gmail.com>
2025-01-31 16:34:23 -0500

    add new putch patch delete methods

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -17,6 +16,24 @@ export class CarsController {
     @Get(':id')
     getCardById( @Param('id', ParseIntPipe) id: number){
         return this.carsService.findOne(+id);
+    }
+
+    @Post()
+    createCar( @Body() body: any){
+        return body;
+    }
+
+    @Patch(':id')
+    updatedCar(@Body() body:any){
+        return body;
+    }
+
+    @Delete(':id')
+    deleteCar(@Param('id', ParseIntPipe) id: number){
+        return {
+            method: 'delete',
+            id
+        }
     }
 }
 
